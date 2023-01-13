f102d26dc97a63db5cc483e0d528c5c0c77f01a5
VitaliyLenin <vitaliylenin77@gmail.com>
2023-01-13 23:18:34 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
 const { username, tag, location, avatar, stats } = user;
 
@@ -16,7 +18,8 @@ export const App = () => {
         stats={stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      {/* <Statistics stats={data} /> */}
+      <Statistics stats={data} />
+      <FriendList friends={friends} />;
     </div>
   );
 };
